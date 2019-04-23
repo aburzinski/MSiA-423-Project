d181faf5ec0c151713e08d772aca6fe1349108d2
ajb6207 <ajb6207@msia423.analytics.northwestern.edu>
2019-04-23 18:31:22 -0500

    Added silentDelete to helpers.py

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,3 +1,12 @@
 import logging
+import os
+import errno
  
 logger = logging.getLogger(__name__)
+
+def silentRemove(fileName):
+    try:
+        os.remove(fileName)
+    except OSError as e:
+        if e.errno != errno.ENOENT:  # File not found error
+            raise
