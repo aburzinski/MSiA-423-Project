272884970c4c8fa6ada326815f2f4fa10a3680ef
ajb6207 <ajb6207@msia423.analytics.northwestern.edu>
2019-04-23 19:03:02 -0500

    Added logging to helpers.py

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,12 +1,19 @@
 import logging
 import os
 import errno
- 
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(name)s - %(levelname)s - %(asctime)s - %(message)s'
+) 
+
 logger = logging.getLogger(__name__)
 
 def silentRemove(fileName):
     try:
         os.remove(fileName)
+	logger.debug('Removed file ' + filename)
     except OSError as e:
         if e.errno != errno.ENOENT:  # File not found error
             raise
+	else:
+	    logger.debug('File not deleted since it doesn't exist')
