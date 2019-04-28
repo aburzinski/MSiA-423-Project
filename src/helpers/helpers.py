d3fdd9c4a020da6aa189006ad38a9c196742ead0
Alex Burzinski <aburzinski2@gmail.com>
2019-04-28 12:15:32 -0500

    Added silentCreateDir function to helpers

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -9,12 +9,19 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 def silentRemove(fileName):
+    """ Delete file if it exists, other don't do anything """
     try:
         os.remove(fileName)
-        logger.debug('Removed file ' + filename)
+        logger.debug('Removed file ' + fileName)
     except OSError as e:
         if e.errno != errno.ENOENT:  # File not found error
             raise
         else:
             logger.debug('File not deleted since it doesn\'t exist')
 
+
+def silentCreateDir(dirName):
+    """ Create directory if it doesn't already exist """
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+        logger.debug('Created directory ' + dirName)
