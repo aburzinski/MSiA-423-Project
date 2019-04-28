bed0fc5e21370cfb907920d90e6ddd38bda59a9b
Alex Burzinski <aburzinski2@gmail.com>
2019-04-28 15:43:03 -0500

    Started working on generate_features.py

--- src/generate_features.py
+++ src/generate_features.py
@@ -1,3 +1,19 @@
 import logging
+import os
+import pandas as pd
  
 logger = logging.getLogger(__name__)
+
+currentDir = os.path.dirname(__file__)
+parentDir = os.path.split(currentDir)[0]
+
+cyYoungPath = os.path.join(parentDir, 'data', 'auxiliary')
+cyYoung = pd.read_csv(os.path.join(cyYoungPath, 'cyYoungWinners.csv'), quotechar='"', encoding='latin-1')
+
+playersPath = os.path.join(parentDir, 'data', 'historical')
+players = pd.read_csv(os.path.join(playersPath, 'players.csv'), quotechar='"', encoding='latin-1')
+
+merged = cyYoung.merge(players, how='left', left_on='Winner', right_on='name_display_first_last')
+
+print(merged[merged['name_full'].isnull()])
+
