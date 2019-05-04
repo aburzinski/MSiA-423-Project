f51eb5fd6a8718af45d2869931799806dc5ab84d
Alex Burzinski <aburzinski2@gmail.com>
2019-05-04 13:23:54 -0500

    Added name main if statement

--- src/write_to_cloud.py
+++ src/write_to_cloud.py
@@ -13,25 +13,26 @@ config.read(os.path.join(parentDir, 'azureConfig.ini'))
 logging.config.fileConfig(os.path.join(parentDir, 'config', 'logging', 'local.conf'))
 logger = logging.getLogger(__name__)
 
-session = boto3.Session(
-    aws_access_key_id=config['DEFAULT']['aws_access_key_id'],
-    aws_secret_access_key=config['DEFAULT']['aws_secret_access_key']
-)
+if __name__ == '__main__':
+    session  = boto3.Session(
+        aws_access_key_id=config['DEFAULT']['aws_access_key_id'],
+        aws_secret_access_key=config['DEFAULT']['aws_secret_access_key']
+    )
 
-bucketName = 'nw-alexburzinski'
-bucketPath = 'data/historical/'
+    bucketName = 'nw-alexburzinski'
+    bucketPath = 'data/historical/'
 
-s3 = session.resource('s3')
-bucket = s3.Bucket(bucketName)
-logger.debug('Writing to the s3 bucket ' + bucketName)
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucketName)
+    logger.debug('Writing to the s3 bucket ' + bucketName)
 
-files = [f for f in os.listdir(readFromDir)
-    if os.path.isfile(os.path.join(readFromDir, f))]
+    files = [f for f in os.listdir(readFromDir)
+        if os.path.isfile(os.path.join(readFromDir, f))]
 
-logger.debug('Writing the following files to s3: ' + files)
+    logger.debug('Writing the following files to s3: ' + files)
 
-for f in files:
-    with open(os.path.join(readFromDir, f), 'rb') as data:
-        bucket.put_object(Key=bucketPath + f, Body=data)
-    data.close()
-    logger.debug('Wrote {} to s3 successfully'.format(f))
+    for f in files:
+        with open(os.path.join(readFromDir, f), 'rb') as data:
+            bucket.put_object(Key=bucketPath + f, Body=data)
+        data.close()
+        logger.debug('Wrote {} to s3 successfully'.format(f))
