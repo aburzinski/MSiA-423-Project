4075394c438ba894016c4a0e47474b576bce7121
Alex Burzinski <aburzinski2@gmail.com>
2019-05-12 14:30:17 -0500

    Updated databse schema

--- models/mlb_database/create_database.py
+++ models/mlb_database/create_database.py
@@ -5,7 +5,7 @@ import os
 import logging.config
 from config import config
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String, Float, MetaData
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy as sql
 import src.helpers.helpers as h
@@ -20,6 +20,15 @@ class Player(Base):
     __tablename__ = 'player'
     id = Column(Integer, primary_key=True)
     playerName = Column(String(100), unique=False, nullable=False)
+    birthCity = Column(String(100), unique=False, nullable=True)
+    birthState = Column(String(10), unique=False, nullable=True)
+    birthCountry = Column(String(100), unique=False, nullable=True)
+    age = Column(Integer, unique=False, nullable=True)
+    height = Column(String(10), unique=False, nullable=True)
+    weight = Column(String(10), unique=False, nullable=True)
+    debutDate = Column(String(20), unique=False, nullable=True)
+    position = Column(String(20), unique=False, nullable=True)
+    currentTeamId = Column(Integer, unique=False, nullable=True)
 
     def __repr__(self):
         return '<Player %r>' % self.playerName
@@ -29,9 +38,35 @@ class Team(Base):
     __tablename__ = 'team'
     id = Column(Integer, primary_key=True)
     teamName = Column(String(100), unique=False, nullable=False)
+    venueName = Column(String(100), unique=False, nullable=True)
+    city = Column(String(100), unique=False, nullable=True)
+    state = Column(String(100), unique=False, nullable=True)
+    league = Column(String(10), unique=False, nullable=True)
+    division = Column(String(10), unique=False, nullable=True)
+    yearFounded = Column(String(10), unique=False, nullable=True)
 
     def __repr__(self):
         return '<Team %r>' % self.teamName
+
+class CurrentStats(Base):
+    """Create a table to hold current stats and predictions"""
+    __tablename__ = 'currentStats'
+    playerId = Column(Integer, primary_key=True)
+    teamId = Column(Integer, unique=False, nullable=False)
+    isPitcher = Column(Integer, unique=False, nullable=False)
+    homeRuns = Column(Integer, unique=False, nullable=True)
+    runsBattedIn = Column(Integer, unique=False, nullable=True)
+    onBasePlusSlug = Column(Float, unique=False, nullable=True)
+    atBats = Column(Integer, unique=False, nullable=True)
+    earnedRunAverage = Column(Float, unique=False, nullable=True)
+    saves = Column(Integer, unique=False, nullable=True)
+    strikeOuts = Column(Integer, unique=False, nullable=True)
+    inningsPitched = Column(Float, unique=False, nullable=True)
+    winPercentage = Column(Float, unique=False, nullable=True)
+    mvpLikelihood = Column(Float, unique=False, nullable=True)
+    mvpRank = Column(Integer, unique=False, nullable=True)
+    cyYoungLikelihood = Column(Float, unique=False, nullable=True)
+    cyYoungRank = Column(Integer, unique=False, nullable=True)
 
 if __name__ == '__main__':
 
@@ -43,5 +78,8 @@ if __name__ == '__main__':
     engine_string = config.SQLALCHEMY_DATABASE_URI
     engine = sql.create_engine(engine_string)
 
-    Base.metadata.create_all(engine)
+    # Drop all tables if they already exist
+    Base.metadata.drop_all(bind=engine)
+
+    Base.metadata.create_all(bind=engine)
     logger.info('MLB database created successfully')
