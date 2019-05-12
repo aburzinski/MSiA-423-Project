e9a99db3b65a99ec0674f4c3d8163023762aa97f
Alex Burzinski <aburzinski2@gmail.com>
2019-05-12 16:04:52 -0500

    Got player ingestion working

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -6,26 +6,36 @@ import logging.config
 from config import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import boto3
 import src.helpers.helpers as h
+import src.helpers.azure as ah
 
 parentDir = config.PROJECT_ROOT_DIR
 logging.config.fileConfig(config.LOGGING_CONFIG_FILE, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-from models.mlb_database.create_database import Base, Player, Team
+from models.mlb_database.create_database import Base
+import src.ingestion_scripts.Player as player
 
-# Don't need to check for dir, that is done on database creation
-# if config.SQLALCHEMY_TYPE == 'sqlite':
-#     h.silentCreateDir(config.SQLALCHEMY_SQLITE_DIR)
+if __name__ == '__main__':
+    engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+    Base.metadata.bind = engine
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    dbsession = DBSession()
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+    s3session  = boto3.Session(
+        aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+    )
 
-new_player = Player(id=123, playerName='new player')
-session.add(new_player)
-session.commit()
+    bucketName = config.AWS_S3_BUCKET_NAME
+    bucketPath = 'data/{}/'.format('historical')
 
-logging.info('Added player to database successfully')
+    s3 = s3session.client('s3')
+    logger.debug('Connected to s3 successfully')
+
+    logger.info('Reading from the s3 bucket ' + bucketName)
+    playerFile = ah.readFileFromS3('players.csv', s3, bucketName, bucketPath)
+
+    player.ingestPlayers(playerFile, dbsession, truncate=True)
---
+++ src/ingestion_scripts/Player.py
@@ -0,0 +1,72 @@
+import sys
+sys.path.append(r'C:\Users\aburz\OneDrive\Documents\Northwestern\Classes\MSiA 423\Project\MSiA-423-Project')
+
+import logging.config
+from config import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import src.helpers.helpers as h
+import src.helpers.azure as ah
+
+parentDir = config.PROJECT_ROOT_DIR
+logging.config.fileConfig(config.LOGGING_CONFIG_FILE, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+from models.mlb_database.create_database import Base, Player
+
+def ingestPlayers(s3File, session, truncate=True):
+    
+    if truncate:
+        session.query(Player).delete()
+
+    # Column numbers are indexed from one
+    playerSchema = {
+        'id': 24,
+        'playerName': 3,
+        'birthCity': 40,
+        'birthState': 30,
+        'birthCountry': 1,
+        'age': 7,
+        'heightInches': 5,
+        'heightFeet': 10,
+        'weight': 31,
+        'debutDate': 11,
+        'position': 20,
+        'currentTeamId': 27
+    }
+
+    playerCount = 0
+
+    for line in s3File.splitlines(False)[1:]:
+        line = line[1:][:-1].split('","')
+
+        # Need to subtract one to index from zero
+        playerId = int(line[playerSchema['id'] - 1])
+        playerName = line[playerSchema['playerName'] - 1]
+        birthCity = line[playerSchema['birthCity'] - 1]
+        birthState = line[playerSchema['birthState'] - 1]
+        birthCountry = line[playerSchema['birthCountry'] - 1]
+        age = int(line[playerSchema['age'] - 1])
+        height = line[playerSchema['heightFeet'] - 1] + '\'' + line[playerSchema['heightInches'] - 1] + '"'
+        weight = int(line[playerSchema['weight'] - 1])
+        debutDate = h.textParseDate(line[playerSchema['debutDate'] - 1])
+        position = line[playerSchema['position'] - 1]
+        currentTeamId = int(line[playerSchema['currentTeamId'] - 1])
+        
+        new_player = Player(id=playerId,
+            playerName=playerName,
+            birthCity=birthCity,
+            birthState=birthState,
+            birthCountry=birthCountry,
+            age=age,
+            height=height,
+            weight=weight,
+            debutDate=debutDate,
+            position=position,
+            currentTeamId=currentTeamId
+        )
+        session.add(new_player)
+        playerCount += 1
+
+    session.commit()
+    logging.info('Added {} players to database successfully'.format(str(playerCount)))
