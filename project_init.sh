7f9e1f68e34c20c9f975cdb81ecfa6fdd51cccd3
Alex Burzinski <aburzinski2@gmail.com>
2019-05-14 16:59:59 -0500

    Fixed script bugs

--- project_init.sh
+++ project_init.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-# Add project path as an environment variable
-$path="/nfs/home/user/project"
+# To run this, use source project_init.sh
 
-export PYTHONPATH=$path
+# Add project path as an environment variable
+export PYTHONPATH='/nfs/home/user/project'
 
 # Add S3 bucket and keys
-$aws_bucket_name=""
-$aws_access_key_id=""
-$aws_secret_access_key=""
-
-export AWS_BUCKET_NAME=$aws_bucket_name
-export AWS_ACCESS_KEY_ID=$aws_access_key_id
-export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+export AWS_BUCKET_NAME=''
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
 
 # Add RDS MySQL login info
-
-$mysql_host=""
-$mysql_user=""
-$mysql_password=""
-
-export MYSQL_HOST=$mysql_host
-export MYSQL_USER=$mysql_user
-export MYSQL_PASSWORD=$mysql_password
+export MYSQL_HOST=''
+export MYSQL_USER=''
+export MYSQL_PASSWORD=''
