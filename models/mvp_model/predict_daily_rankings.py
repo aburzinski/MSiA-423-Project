310c517ea37540b3d143bcff8189178244e2a7b6
Alex Burzinski <aburzinski2@gmail.com>
2019-05-22 13:35:47 -0500

    Added if name == main

--- models/mvp_model/predict_daily_rankings.py
+++ models/mvp_model/predict_daily_rankings.py
@@ -10,22 +10,29 @@ import pickle
 logging.config.fileConfig(config.LOGGING_CONFIG_FILE, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-pitching = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'pitchingProjected.csv'), encoding = 'latin')
-hitting = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'hittingProjected.csv'), encoding = 'latin')
-players = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'historical', 'players.csv'), encoding = 'latin')
+def makePredictions(modelData):
+    """Make predictions based on the input data, and rank them
 
-with open(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'model_files', 'mvp.model'), 'rb') as f:
-    lr = pickle.load(f)
-f.close()
+    Returns:
+        A dataframe with the player_id, prediction, and rank
+    """
+    predictData = modelData
+    predictData = predictData.loc[:, predictData.columns != 'player_id']
 
-modelData = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'features', 'mvpFeaturesProjected.csv'))
+    modelData['prediction'] = lr.predict_proba(predictData)[:,1]
+    modelData['rank'] = modelData['prediction'].rank(method='dense', ascending=False)
+    modelData = modelData[['player_id', 'prediction', 'rank']]
 
-predictData = modelData
-predictData = predictData.loc[:, predictData.columns != 'player_id']
+    return modelData
 
-modelData['prediction'] = lr.predict_proba(predictData)[:,1]
-modelData['rank'] = modelData['prediction'].rank(method='dense', ascending=False)
-modelData = modelData[['player_id', 'prediction', 'rank']]
+if __name__ == '__main__':
+    with open(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'model_files', 'mvp.model'), 'rb') as f:
+        lr = pickle.load(f)
+    f.close()
 
-modelData.to_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'mvpPredictions.csv'), index=False)
-logger.info('{} predictions made'.format(modelData.shape[0]))
+    modelData = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'features', 'mvpFeaturesProjected.csv'))
+
+    modelData = makePredictions(modelData)
+    
+    modelData.to_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'mvpPredictions.csv'), index=False)
+    logger.info('{} predictions made'.format(modelData.shape[0]))
