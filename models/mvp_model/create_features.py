470f1da5893cade709dff5dc4dfcd2e35dfb848b
Alex Burzinski <aburzinski2@gmail.com>
2019-05-22 13:05:21 -0500

    Added if name == main

--- models/mvp_model/create_features.py
+++ models/mvp_model/create_features.py
@@ -22,80 +22,93 @@ args = parser.parse_args()
 if args.featureType not in ['historical', 'projected']:
     raise ValueError('The directory argument must be either "historical" or "projected"')
 
-# Specify columns to later check for missing values
-pitching = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', args.featureType, 'pitching{}.csv'.format(args.featureType)),
-    encoding = 'latin', dtype = {'ppa': str, 'obp': str, 'era': str, 'whip': str})
-hitting = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', args.featureType, 'hitting{}.csv'.format(args.featureType)),
-    encoding = 'latin', dtype = {'slg': str, 'obp': str})
-players = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'historical', 'players.csv'), encoding = 'latin')
-mvp = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'auxiliary', 'mvpWinners.csv'), encoding = 'latin')
+def readFeatures():
+    """Read the raw data from local fs or S3, and format it correctly for creating features"""
+    # Specify columns to later check for missing values
+    pitching = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', args.featureType, 'pitching{}.csv'.format(args.featureType)),
+        encoding = 'latin', dtype = {'ppa': str, 'obp': str, 'era': str, 'whip': str})
+    hitting = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', args.featureType, 'hitting{}.csv'.format(args.featureType)),
+        encoding = 'latin', dtype = {'slg': str, 'obp': str})
+    players = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'historical', 'players.csv'), encoding = 'latin')
+    mvp = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'auxiliary', 'mvpWinners.csv'), encoding = 'latin')
 
-merged = players.merge(mvp, how='left', left_on='name_display_first_last', right_on='Winner')
-if args.featureType == 'projected':
-    merged['season'] = config.CURRENT_SEASON
-    pitching['season'] = config.CURRENT_SEASON
-    hitting['season'] = config.CURRENT_SEASON
+    merged = players.merge(mvp, how='left', left_on='name_display_first_last', right_on='Winner')
+    if args.featureType == 'projected':
+        merged['season'] = config.CURRENT_SEASON
+        pitching['season'] = config.CURRENT_SEASON
+        hitting['season'] = config.CURRENT_SEASON
 
-# Need to rename the columns because the historical data will be rename
-# during the join, by projected data will not
-pitching.rename(columns={
-    'h': 'h_x',
-    'hr': 'hr_x',
-    'bb': 'bb_x',
-    'so': 'so_x'
-}, inplace=True)
+    # Need to rename the columns because the historical data will be rename
+    # during the join, by projected data will not
+    pitching.rename(columns={
+        'h': 'h_x',
+        'hr': 'hr_x',
+        'bb': 'bb_x',
+        'so': 'so_x'
+    }, inplace=True)
 
-hitting.rename(columns={
-    'h': 'h_y',
-    'hr': 'hr_y',
-    'bb': 'bb_y',
-    'so': 'so_y',
-    'ab': 'ab_y',
-    'obp': 'obp_y',
-    'slg': 'slg_y'
-}, inplace=True)
+    hitting.rename(columns={
+        'h': 'h_y',
+        'hr': 'hr_y',
+        'bb': 'bb_y',
+        'so': 'so_y',
+        'ab': 'ab_y',
+        'obp': 'obp_y',
+        'slg': 'slg_y'
+    }, inplace=True)
 
-stats = pitching.merge(hitting, how='outer', left_on=['player_id', 'season'], right_on=['player_id', 'season'])
-merged = stats.merge(merged, how = 'left', left_on=['player_id','season'], right_on=['player_id', 'Year'])
+    stats = pitching.merge(hitting, how='outer', left_on=['player_id', 'season'], right_on=['player_id', 'season'])
+    merged = stats.merge(merged, how = 'left', left_on=['player_id','season'], right_on=['player_id', 'Year'])
 
-minimumInningsPitched = 10
-minimumAtBats = 10
-merged = merged[(merged['ip'] > minimumInningsPitched) | (merged['ab_y'] > minimumAtBats)]
+    return merged
 
-merged['sv_pct'] = merged['sv']/merged['svo']
-merged['win_pct'] = merged['w']/(merged['w'] + merged['l'])
-merged['hits_9'] = mh.nineInningNormalize(merged, 'h_x')
-merged['hrs_9'] = mh.nineInningNormalize(merged, 'hr_x')
-merged['bbs_9'] = mh.nineInningNormalize(merged, 'bb_x')
-merged['ks_9'] = mh.nineInningNormalize(merged, 'so_x')
-merged['ers_9'] = mh.nineInningNormalize(merged, 'er')
+def cleanFeatures(merged):
+    """Clean and normalize the feature data"""
+    minimumInningsPitched = 10
+    minimumAtBats = 10
+    merged = merged[(merged['ip'] > minimumInningsPitched) | (merged['ab_y'] > minimumAtBats)]
 
-merged['hit_ab'] = mh.atBatNormalize(merged, 'h_y', 'ab_y')
-merged['hr_ab'] = mh.atBatNormalize(merged, 'hr_y', 'ab_y')
-merged['rbi_ab'] = mh.atBatNormalize(merged, 'rbi', 'ab_y')
-merged['bb_ab'] = mh.atBatNormalize(merged, 'bb_y', 'ab_y')
-merged['k_ab'] = mh.atBatNormalize(merged, 'so_y', 'ab_y')
+    merged.loc[:, 'sv_pct'] = merged['sv']/merged['svo']
+    merged.loc[:, 'win_pct'] = merged['w']/(merged['w'] + merged['l'])
+    merged.loc[:, 'hits_9'] = mh.nineInningNormalize(merged, 'h_x')
+    merged.loc[:, 'hrs_9'] = mh.nineInningNormalize(merged, 'hr_x')
+    merged.loc[:, 'bbs_9'] = mh.nineInningNormalize(merged, 'bb_x')
+    merged.loc[:, 'ks_9'] = mh.nineInningNormalize(merged, 'so_x')
+    merged.loc[:, 'ers_9'] = mh.nineInningNormalize(merged, 'er')
 
-if args.featureType == 'historical':
-    merged['is_winner'] = merged['Winner'].apply(lambda x: 0 if isinstance(x, float) else 1)
-    modelData = merged[['hits_9', 'hrs_9', 'bbs_9', 'ks_9', 'ers_9', 'sv_pct', 'win_pct', 'era', 'whip', 'hit_ab', 'hr_ab', 'rbi_ab', 'bb_ab', 'k_ab', 'slg_y', 'obp_y', 'is_winner']]
-elif args.featureType == 'projected':
-    modelData = merged[['player_id', 'hits_9', 'hrs_9', 'bbs_9', 'ks_9', 'ers_9', 'sv_pct', 'win_pct', 'era', 'whip', 'hit_ab', 'hr_ab', 'rbi_ab', 'bb_ab', 'k_ab', 'slg_y', 'obp_y']]
+    merged.loc[:, 'hit_ab'] = mh.atBatNormalize(merged, 'h_y', 'ab_y')
+    merged.loc[:, 'hr_ab'] = mh.atBatNormalize(merged, 'hr_y', 'ab_y')
+    merged.loc[:, 'rbi_ab'] = mh.atBatNormalize(merged, 'rbi', 'ab_y')
+    merged.loc[:, 'bb_ab'] = mh.atBatNormalize(merged, 'bb_y', 'ab_y')
+    merged.loc[:, 'k_ab'] = mh.atBatNormalize(merged, 'so_y', 'ab_y')
 
-modelData.is_copy = False
+    if args.featureType == 'historical':
+        merged['is_winner'] = merged['Winner'].apply(lambda x: 0 if isinstance(x, float) else 1)
+        modelData = merged[['hits_9', 'hrs_9', 'bbs_9', 'ks_9', 'ers_9', 'sv_pct', 'win_pct', 'era', 'whip', 'hit_ab', 'hr_ab', 'rbi_ab', 'bb_ab', 'k_ab', 'slg_y', 'obp_y', 'is_winner']]
+    elif args.featureType == 'projected':
+        modelData = merged[['player_id', 'hits_9', 'hrs_9', 'bbs_9', 'ks_9', 'ers_9', 'sv_pct', 'win_pct', 'era', 'whip', 'hit_ab', 'hr_ab', 'rbi_ab', 'bb_ab', 'k_ab', 'slg_y', 'obp_y']]
 
-modelData.loc[modelData['era'] == '-.--', 'era'] = 0.0
-modelData.loc[modelData['era'] == '*.**', 'era'] = 0.0
-modelData.loc[modelData['whip'] == '-.--', 'whip'] = 0.0
-modelData.loc[modelData['whip'] == '*.**', 'whip'] = 0.0
+    modelData.is_copy = False
 
-modelData.loc[modelData['slg_y'] == '.---', 'slg_y'] = 0.0
-modelData.loc[modelData['obp_y'] == '.---', 'obp_y'] = 0.0
+    modelData.loc[modelData['era'] == '-.--', 'era'] = 0.0
+    modelData.loc[modelData['era'] == '*.**', 'era'] = 0.0
+    modelData.loc[modelData['whip'] == '-.--', 'whip'] = 0.0
+    modelData.loc[modelData['whip'] == '*.**', 'whip'] = 0.0
 
-modelData = modelData.replace([np.inf, -np.inf], np.nan).fillna(0)
+    modelData.loc[modelData['slg_y'] == '.---', 'slg_y'] = 0.0
+    modelData.loc[modelData['obp_y'] == '.---', 'obp_y'] = 0.0
 
-writeToDir = os.path.join(config.PROJECT_ROOT_DIR, 'data', 'features')
-h.silentCreateDir(writeToDir)
-modelData.to_csv(os.path.join(writeToDir, 'mvpFeatures{}.csv'.format(args.featureType)), index=False)
+    modelData = modelData.replace([np.inf, -np.inf], np.nan).fillna(0)
 
-logger.info('A file with {} rows and {} columns was created'.format(modelData.shape[0], modelData.shape[1]))
+    return modelData
+
+if __name__ == '__main__':
+
+    merged = readFeatures()
+    modelData = cleanFeatures(merged)
+
+    writeToDir = os.path.join(config.PROJECT_ROOT_DIR, 'data', 'features')
+    h.silentCreateDir(writeToDir)
+    modelData.to_csv(os.path.join(writeToDir, 'mvpFeatures{}.csv'.format(args.featureType)), index=False)
+
+    logger.info('A file with {} rows and {} columns was created'.format(modelData.shape[0], modelData.shape[1]))
