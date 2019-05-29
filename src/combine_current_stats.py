43de4d2e338aa5e0827c7bcb463beb0756bf2eac
Alex Burzinski <aburzinski2@gmail.com>
2019-05-29 15:35:33 -0500

    Fixed missing data bug

--- src/combine_current_stats.py
+++ src/combine_current_stats.py
@@ -19,13 +19,16 @@ if __name__ == '__main__':
 
     cyYoungStats = pd.read_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'cyYoungPredictions.csv'))
     cyYoungStats.rename(columns={
-        'prediciton': 'cyYoungLikelihood',
+        'prediction': 'cyYoungLikelihood',
         'rank': 'cyYoungRank'
     }, inplace=True)
 
     merged = mvpStats.merge(cyYoungStats, how='left', on='player_id')
     logger.debug('Merging files of size {} and size {} to a file of size {} by {}'.format(mvpStats.shape[0],
         cyYoungStats.shape[0], merged.shape[0], merged.shape[1]))
+
+    merged['cyYoungLikelihood'] = merged['cyYoungLikelihood'].fillna(0.0)
+    merged['cyYoungRank'] = merged['cyYoungRank'].fillna(99999)
 
     merged.to_csv(os.path.join(config.PROJECT_ROOT_DIR, 'data', 'projected', 'currentStats.csv'), index=False)
 
